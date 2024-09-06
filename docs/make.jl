2aa672488afb2e35ac3e74deabcb080e7210e904
Christian Merdon <merdon@wias-berlin.de>
2024-09-06 14:31:10 +0200

    fixed repolink in Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -7,7 +7,7 @@ makedocs(
 		modules = [StrainedBandstructures],
 		sitename = "StrainedBandStructures.jl",
 		authors = "Patricio Farrell, Yiannis Hadjimichael, Christian Merdon",
-		repo = "github.com/hadjimy/StrainedBandstructures",
+		format = Documenter.HTML(; repolink = "https://github.com/hadjimy/StrainedBandstructures", mathengine = MathJax3()),
 		clean = false,
 		source  = "src",
 		build   = "build",
